1cd1a56a137c8e0b5c3610d2bd4a9e38c2c93e26
DushynatBhambore <goodbyebeautiful07@gmail.com>
2023-07-28 17:15:37 +0530

    prime number code 2

---
+++ src/main/java/com/bootcoding/java/Exprime.java
@@ -0,0 +1,24 @@
+package com.bootcoding.java;
+import java.util.*;
+public class Exprime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a nummber =");
+        int n = sc.nextInt();
+        boolean isprime = true;
+
+        for(int i=2;i<=n-1;i++){
+            if(n%i==0) {
+                isprime = false;
+            }
+        }
+        if(isprime==true)
+        {
+            System.out.println(n+" is prime number");
+        }
+        else
+        {
+            System.out.println(n+" is not a prime number");
+        }
+    }
+}
